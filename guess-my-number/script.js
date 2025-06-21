90b2c8a46e232d739c1e0c2892f28e2bc631a992
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-06-21 19:06:40 +0200

    Create click event for check button

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,1 +1,16 @@
+/* Logic for handling Guess My Number Game */
+
+// Enabled Strict Mode
 'use strict';
+
+/** ====================
+ * Handling Click Events
+ * ===================== */
+
+// Retrieve the button with class name "check" from the document
+const checkBtn = document.querySelector('.check');
+
+// Listen for the click event on checkBtn
+checkBtn.addEventListener('click', (e) => {}
+
+)
