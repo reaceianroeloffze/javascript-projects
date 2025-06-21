0332c428dd4c52c8ef9aeda26bb24092fd36447d
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-06-21 19:40:29 +0200

    Add logic for when no guess is made

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -10,7 +10,30 @@
 // Retrieve the button with class name "check" from the document
 const checkBtn = document.querySelector('.check');
 
-// Listen for the click event on checkBtn
-checkBtn.addEventListener('click', (e) => {}
+// Get the paragraph element that prints a message when the user makes a guess
+const guessMsg = document.querySelector('.message');
 
+// Listen for the click event on checkBtn
+checkBtn.addEventListener('click', () => {
+        // Retrieve the guessed value input by a user and convert it to a number
+        const inputGuess = Number(document.querySelector('.guess').value);
+
+        // If no guess is made and the check button is clicked, print a message notifying the user
+        if (!inputGuess) {
+            guessMsg.textContent = '🚫 No number guessed!';
+        }
+    }
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
