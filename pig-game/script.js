384cb023f4dcd482b449a48d0eb871540c02fb03
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-02 20:07:57 +0200

    Add gameplay status

--- pig-game/script.js
+++ pig-game/script.js
@@ -56,45 +56,50 @@ const switchToNextPlayer = function() {
 
 // Roll Dice Button Functionality
 rollDiceBtn.addEventListener('click', () => {
-	// Generate a random number between 1 & 6
-	const diceNumber = Math.trunc(Math.random() * 6) + 1;
+	if (canPlay) {
+		// Generate a random number between 1 & 6
+		const diceNumber = Math.trunc(Math.random() * 6) + 1;
 
-	// Remove the dice__hidden class if present
-	if (diceElem.classList.contains('dice__hidden')) {
-		diceElem.classList.remove('dice__hidden');
-	}
-	// display the rolled number
-	diceImg.src = `dice-${diceNumber}.png`;
+		// Remove the dice__hidden class if present
+		if (diceElem.classList.contains('dice__hidden')) {
+			diceElem.classList.remove('dice__hidden');
+		}
+		// display the rolled number
+		diceImg.src = `dice-${diceNumber}.png`;
 
-	// Check if the diceNumber = 1
-	if (diceNumber !== 1) {
-		// Check which player is active and apply dice roll to the current score
-		playerCurrentScore += diceNumber;
-		document.querySelector(`#current--${activePlayer}`).innerText = playerCurrentScore;
-	} else {
-		// Switch to the next player and reset the current player's current score
-		switchToNextPlayer();
+		// Check if the diceNumber = 1
+		if (diceNumber !== 1) {
+			// Check which player is active and apply dice roll to the current score
+			playerCurrentScore += diceNumber;
+			document.querySelector(`#current--${activePlayer}`).innerText = playerCurrentScore;
+		} else {
+			// Switch to the next player and reset the current player's current score
+			switchToNextPlayer();
+		}
 	}
 });
 
 // Hold Score Button Functionality
 holdScoreBtn.addEventListener('click', () => {
-	// Add the current score to the active player's overall score
-	scores[activePlayer] += playerCurrentScore;
-	document.querySelector(`#score--${activePlayer}`).innerText = scores[activePlayer];
+	if (canPlay) {
+		// Add the current score to the active player's overall score
+		scores[activePlayer] += playerCurrentScore;
+		document.querySelector(`#score--${activePlayer}`).innerText = scores[activePlayer];
 
-	// Check to see if a player has reached a score of 100 or higher
-	if (scores[activePlayer] >= 100) {
-		// End/finish the game and announce a winner
-		document.querySelector(`.player--${activePlayer}`)
-			.classList.add('player--winner');
-		document
-			.querySelector(`.player--${activePlayer}`)
-			.classList.remove('player--active');
-		diceElem.classList.add('dice__hidden');
-	} else {
-		// Switch players and reset current player score
-		switchToNextPlayer();
+		// Check to see if a player has reached a score of 100 or higher
+		if (scores[activePlayer] >= 100) {
+			canPlay = false;
+			// End/finish the game and announce a winner
+			document.querySelector(`.player--${activePlayer}`)
+				.classList.add('player--winner');
+			document
+				.querySelector(`.player--${activePlayer}`)
+				.classList.remove('player--active');
+			diceElem.classList.add('dice__hidden');
+		} else {
+			// Switch players and reset current player score
+			switchToNextPlayer();
+		}
 	}
 });
 
