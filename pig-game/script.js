cfa4cce957a129d8cbb5e700ffdb4f960f37de3b
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-02 17:23:47 +0200

    Add Hold Score Button Event Logic

--- pig-game/script.js
+++ pig-game/script.js
@@ -47,20 +47,23 @@ rollDiceBtn.addEventListener('click', () => {
 	}
 });
 
+// Hold Score Button Functionality
+holdScoreBtn.addEventListener('click', () => {
+	// Add the current score to the active player's overall score
+	scores[activePlayer] += playerCurrentScore;
+	document.querySelector(`#score--${activePlayer}`).innerText = scores[activePlayer];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// Check to see if a player has reached a score of 100 or higher
+	if (scores[activePlayer] >= 20) {
+		// End/finish the game and announce a winner
+		document.querySelector(`.player--${activePlayer}`)
+			.classList.add('player--winner');
+		document
+			.querySelector(`.player--${activePlayer}`)
+			.classList.remove('player--active');
+		diceElem.classList.add('dice__hidden');
+	} else {
+		// Switch players and reset current player score
+		switchToNextPlayer();
+	}
+});
