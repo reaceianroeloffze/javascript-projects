527c77850f87471c428eae448d53810376f696e3
Reace Ian Roeloffze <righawkroeloffze@gmail.com>
2025-07-02 16:10:12 +0200

    Select necessary DOM elements

--- pig-game/script.js
+++ pig-game/script.js
@@ -3,7 +3,18 @@
 // Enable Strict Mode
 'use strict';
 
-
+// Select all necessary document elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const playerScore0Elem = document.querySelector('#score--0');
+const playerScore1Elem = document.querySelector('#score--1');
+const diceElem = document.querySelector('.dice');
+const newGameBtn = document.querySelector('.btn--new');
+const rollDiceBtn = document.querySelector('.btn--roll');
+const holdScoreBtn = document.querySelector('.btn--hold');
+const diceImg = document.querySelector('.dice');
+const player0CurrentScoreElem = document.querySelector('#current--0');
+const player1CurrentScoreElem = document.querySelector('#current--1');
 
 
 
